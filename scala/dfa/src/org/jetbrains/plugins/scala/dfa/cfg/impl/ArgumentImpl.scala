0e56501631f551f7117749039d9ccb6bd245d95c
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-09-22 15:57:30 +0200

    Added arguments building
     #SCL-15150

---
+++ scala/dfa/src/org/jetbrains/plugins/scala/dfa/cfg/impl/ArgumentImpl.scala
@@ -0,0 +1,9 @@
+package org.jetbrains.plugins.scala.dfa
+package cfg
+package impl
+
+private final class ArgumentImpl[Info](override val argumentName: String) extends ValueImpl[Info] with Argument { this: Value =>
+  override def argumentIndex: Int = valueId
+
+  override protected def asmString: String = s"$valueIdString <- $argumentName [argument $argumentIndex]"
+}
