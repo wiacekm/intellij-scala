220decc141037a8246b4f3e16039af11c828fd54
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-09-24 14:18:52 +0200

    added first version of DataFlowAnalysis
     #SCL-15150

---
+++ scala/dfa/src/org/jetbrains/plugins/scala/dfa/analysis/DataFlowAnalysis.scala
@@ -0,0 +1,99 @@
+package org.jetbrains.plugins.scala.dfa
+package analysis
+
+import org.jetbrains.plugins.scala.dfa.analysis.DataFlowAnalysis.WQItem
+import org.jetbrains.plugins.scala.dfa.analysis.impl.createNodeInstance
+
+import scala.collection.mutable
+
+final class DataFlowAnalysis(val graph: cfg.Graph[_]) {
+  private val items = graph.nodes.zipWithIndex.map { case (node, index) => new WQItem(index, node) }
+  private val workQueue = mutable.PriorityQueue.empty[WQItem](Ordering.by(_.index))
+  private var endStates = List.empty[State]
+
+  private var controller: NodeInstance.Controller = _
+
+  locally {
+    init()
+  }
+
+  private def addToQueue(to: Int, state: State): Unit = {
+    val item = items(to)
+    item.states ::= state
+    if (!item.enqueued) {
+      item.enqueued = true
+      workQueue.enqueue(item)
+    }
+  }
+
+  def hasFinished: Boolean = workQueue.isEmpty
+  def finishedStates: Seq[State] = endStates
+
+  def init(args: Seq[DfAny] = Seq.fill(graph.arguments.size)(DfAny.Top)): Unit = {
+    assert(args.size == graph.arguments.size)
+
+    workQueue.clear()
+    endStates = Nil
+    items.foreach(_.reset())
+
+    controller = new NodeInstance.Controller {
+      override def arguments: Seq[DfAny] = args
+      override def enqueue(index: Int, state: State): Unit = addToQueue(index, state)
+      override def addEndState(state: State): Unit = endStates ::= state
+    }
+
+    addToQueue(0, State.from(graph))
+  }
+
+  def step(): Unit = {
+    val item = workQueue.dequeue()
+    item.enqueued = false
+
+    val instance = item.instance
+    for (state <- item.extractStates()) {
+      assert(state.isActive(instance))
+      instance.process(state, controller)
+    }
+  }
+
+  def run(): Unit =
+    while (!hasFinished) step()
+
+  def inspect(value: cfg.Value): DfAny = {
+    val idx = value.valueId
+    val blockIdx = value.block.index
+
+    assert(hasFinished)
+    assert(graph.values.lift(idx).contains(value))
+
+
+    val valuesFromAllStates =
+      finishedStates.iterator
+        .filter(_.blockActive(blockIdx))
+        .map(_.values(idx))
+
+    join(valuesFromAllStates)
+  }
+}
+
+object DataFlowAnalysis {
+  private class WQItem(val index: Int, val node: cfg.Node) {
+    val shouldDispose: Boolean = true // node.block.incoming.sizeIs <= 1
+    val instance: NodeInstance = createNodeInstance(node)
+    var states: List[State] = Nil
+    var enqueued: Boolean = false
+
+    def extractStates(): Iterator[State] = {
+      val it = states.iterator
+      if (shouldDispose) {
+        states = Nil
+      }
+      it
+    }
+
+    def reset(): Unit = {
+      states = Nil
+      enqueued = false
+    }
+  }
+}
--- scala/dfa/src/org/jetbrains/plugins/scala/dfa/cfg/Graph.scala
+++ scala/dfa/src/org/jetbrains/plugins/scala/dfa/cfg/Graph.scala
@@ -4,13 +4,27 @@ package cfg
 import scala.collection.immutable.ArraySeq
 
 
-final class Graph[+Info](val nodes: ArraySeq[Node], val blocks: ArraySeq[Block], val arguments: ArraySeq[Argument]) {
+final class Graph[Info](val nodes: ArraySeq[Node], val blocks: ArraySeq[Block], val arguments: ArraySeq[Argument]) {
   assert(nodes.nonEmpty)
   assert(blocks.nonEmpty)
   assert(arguments.zip(nodes).forall { case (a, n) => a eq n })
   assert(arguments.forall(_.block eq blocks.head))
 
+  val values: ArraySeq[Value] =
+    nodes.collect { case value: Value => value }
+      .ensuring(_.zipWithIndex.forall { case (value, idx) => value.valueId == idx })
+
+
   def apply(index: Int): Node = nodes(index)
+
+  def nodesForSource(info: Info): Seq[Node] = nodes.filter(_.sourceInfo.contains(info))
+  def valuesForSource(info: Info): Seq[Value] = values.filter(_.sourceInfo.contains(info))
+  def valueForSource(info: Info): Value = {
+    val values = valuesForSource(info)
+    assert(values.nonEmpty)
+    assert(values.size == 1)
+    values.head
+  }
 
   lazy val hasIncomingJump: Set[Node] =
     nodes.collect { case jump: Jumping => jump.target }.toSet
