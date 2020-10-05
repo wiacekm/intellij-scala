9f47a21934d8fd79cad26b80af9e6c6489ab3a0d
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-10-05 18:48:08 +0200

    added initial ReturnsNullInspection
     #SCL-15150

---
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/codeInspection/dfa/ReturnsNullInspection.scala
@@ -0,0 +1,26 @@
+package org.jetbrains.plugins.scala.codeInspection.dfa
+
+import com.intellij.codeInspection._
+import com.intellij.psi.PsiElement
+import org.jetbrains.plugins.scala.codeInspection.ScalaInspectionBundle
+import org.jetbrains.plugins.scala.dfa.analysis.DfaResult
+import org.jetbrains.plugins.scala.extensions._
+import org.jetbrains.plugins.scala.lang.psi.api.ScalaElementVisitor
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
+import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
+
+class ReturnsNullInspection extends LocalInspectionTool {
+  override final def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): ScalaElementVisitor = new ScalaElementVisitor {
+    override def visitFunctionDefinition(fun: ScFunctionDefinition): Unit =
+      for {
+        result <- fun.dataFlowResult
+        expr <- fun.returnUsages
+      } checkReturn(expr, result)
+
+    private def checkReturn(expr: ScExpression, result: DfaResult[PsiElement]): Unit = {
+      if (result.valuesOf(expr).exists(_.canLikelyBeNull)) {
+        holder.registerProblem(expr, ScalaInspectionBundle.message("this.might.return.null.consider.adding.nullable"))
+      }
+    }
+  }
+}
