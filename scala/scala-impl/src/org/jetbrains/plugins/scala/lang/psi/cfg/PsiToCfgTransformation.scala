29913c2a4f5739d2b068024a6134f3b84a9dee85
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-10-09 13:39:34 +0200

    added some more minor transformations
     #SCL-15150

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/cfg/PsiToCfgTransformation.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/cfg/PsiToCfgTransformation.scala
@@ -12,15 +12,16 @@ object PsiToCfgTransformation {
 
   private[cfg] def transformUnsafe(element: PsiElement): PsiGraph = {
     implicit val builder: Builder = cfg.Builder.newBuilder()
-    val transformer = new Transformer(builder)
 
     element match {
       case file: ScalaFile if file.isScriptFile || file.isWorksheetFile =>
+        val transformer = new Transformer(builder, thisVariable = None)
         file.acceptChildren(new ScalaElementVisitor {
           override def visitScalaElement(element: ScalaPsiElement): Unit =
             transformer.transformAny(element)
         })
       case fun: ScFunctionDefinition =>
+        val transformer = new Transformer(builder, thisVariable = Some(builder.addArgument("this", new AnyRef)._1))
         for (param <- fun.parameters) {
           builder.addArgument(param.name, param)
         }
@@ -30,6 +31,7 @@ object PsiToCfgTransformation {
         }
 
       case element: ScalaPsiElement =>
+        val transformer = new Transformer(builder, thisVariable = None)
         transformer.transformAny(element)
 
       case element =>
