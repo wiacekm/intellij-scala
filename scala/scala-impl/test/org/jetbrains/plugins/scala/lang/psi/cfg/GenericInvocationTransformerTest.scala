26413f03c40578800613e897e870161c9d0dcf12
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-10-30 15:08:55 +0100

    added tests for generic method call
     #SCL-15150

---
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/psi/cfg/GenericInvocationTransformerTest.scala
@@ -0,0 +1,74 @@
+package org.jetbrains.plugins.scala.lang.psi.cfg
+
+class GenericInvocationTransformerTest extends TransformerTestBase {
+
+  def test_generic_simple(): Unit = {
+    check(
+      """
+        |def test[A](a: A) = a
+        |
+        |test(100)
+      """.stripMargin,
+      """
+        |%0 <- DfInt(100)
+        |%1 <- call test(%0)
+        |end
+      """.stripMargin
+    )
+  }
+
+  def test_generic_multi_args(): Unit = {
+    check(
+      """
+        |def test[A](a: A)(b: A) = ()
+        |
+        |test(100)(true)
+      """.stripMargin,
+      """
+        |%0 <- DfInt(100)
+        |%1 <- DfTrue
+        |%2 <- call test(%0)(%1)
+        |end
+      """.stripMargin
+    )
+  }
+
+  /*
+  todo: implement auto tupeling
+  def test_autotupeling(): Unit = {
+    check(
+      """
+        |def test[A](a: A) = ()
+        |
+        |test(1, true)
+      """.stripMargin,
+      """
+        |%0 <- call (1, true) tuple
+        |call (%0) test
+      """.stripMargin
+    )
+  }
+  */
+
+  /*
+    todo: implement this
+  def test_typeclass(): Unit = {
+
+    check(
+      """
+        |class TyClass[T]
+        |implicit def ctx: TyClass[Int] = ()
+        |
+        |def test[X: TyClass](x: X) = ()
+        |
+        |test(100)
+      """.stripMargin,
+      """
+        |%0 <- call () ctx
+        |call (100, %0) test
+        |end
+      """.stripMargin
+    )
+  }
+   */
+}
